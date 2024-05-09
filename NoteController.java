fcbca1e08e23c22aaf066a62c0f4f3b957fcb776
KhushiAther450S <163380044+KhushiAther450S@users.noreply.github.com>
2024-05-09 14:22:22 +0530

    Update NoteController.java

--- NoteController.java
+++ NoteController.java
@@ -21,3 +21,50 @@ public class NoteController {
 
     }
 }
+package com.atherenergy.dvp.dto;
+
+import com.atherenergy.dvp.dto.screens.repoDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.Gson;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+
+public class dvpRepoDto {
+
+    @JsonProperty("dashboard")
+    static List<repoDto> repoDetail;
+
+
+    public dvpRepoDto() {
+        List<repoDto> repoDetails = new ArrayList<>();
+        repoDetails.add(new repoDto("dvp_001", "trialDVP", "gen3", " ", "24-02-2024", "approved", "Tarun", "Nataraj"));
+        repoDetails.add(new repoDto("dvp_002", "testDVP", "gen3", " ", "29-02-2024", "updated", "Tarun", "Nataraj"));
+        this.repoDetail = repoDetails;
+
+
+    }
+
+
+    //static repoDto repo=new repoDto("dvp_001","trialDVP","gen3"," ","24-02-2024","approved","Tarun","Nataraj");
+    public static repoDto getDvpById(String dvpId) {
+        for (repoDto dvpTest : repoDetail) {
+            if (dvpTest.getDvpId().equals(dvpId)) {
+                return dvpTest;
+            }
+        }
+        return null;
+    }
+}
+
